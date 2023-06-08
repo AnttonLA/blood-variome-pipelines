ed9c99a0cccba458fa8a45e7100e302268c0cc47
AnttonLA <anttonlamarca@gmail.com>
2023-06-08 14:53:51 +0200

    Refactor extract_remapdb_studies.py so that it can be imported.

--- TF_binding_at_variant/extract_remapdb_studies.py
+++ TF_binding_at_variant/extract_remapdb_studies.py
@@ -3,93 +3,106 @@ import os
 import argparse
 import sys
 
-"""
-This script will take the position of a SNP ('snp_full_pos') and produce a tabix query to the filepath stored in
-'remap_file'. It will then process the output of the tabix query to produce a table with the following columns:
-- study_accession
-- transcription_factor
-- biotype
-- distance_to_snp
 
-"""
+def extract_studies_for_single_snp(chr_pos: str, remap_file: str, tmp_dir: str, output: str) -> None:
+    """
+    This function will take the position of a SNP ('chr_pos') and produce a tabix query to the ReMap metadata file
+    (filepath stored in 'remap_file'). It will then process the output of the tabix query to produce a table with the
+    following columns: study_accession | transcription_factor | biotype | distance_to_snp. The table will be written to
+    a CSV file (filepath stored in 'output').
 
-# Parse arguments
-parser = argparse.ArgumentParser(description='Take the position of a SNP and produce a tabix query to the ReMap '
-                                             'dabase file.')
-parser.add_argument('snp_pos', type=str, help='Position of SNP, e.g. 12:62910804')
-parser.add_argument('-r', '--remap_file', type=str, help='Path to the ReMap database file', required=True)
-parser.add_argument('-o', '--output', type=str, help='Name of the output file', required=True)
+    :param chr_pos: Position of the SNP in the format <chr>:<pos>
+    :param remap_file: Path to ReMap BED file. Requires tabix index file.
+    :param tmp_dir: Directory to store temporary files. If none is specified, temporary files will be stored in the
+    current working directory.
+    :param output: Output file. Must be a CSV file.
+    :return: None
+    """
 
-args = parser.parse_args()
+    # Input sanitation
+    if ':' not in chr_pos:
+        raise ValueError("SNP position must be in the format '<chr>:<position>'")
 
-# Input sanitation
-if not ':' in args.snp_pos:
-    raise ValueError("SNP position must be in the format '<chr>:<position>'")
+    if not chr_pos.split(":")[1].isdigit():
+        raise ValueError("SNP position must be in the format '<chr>:<position>'")
 
-if not args.snp_pos.split(":")[1].isdigit():
-    raise ValueError("SNP position must be in the format '<chr>:<position>'")
+    # Create 'snp_full_pos' variable
+    chrom = chr_pos.split(":")[0]  # Chromosome of the SNP
+    pos = chr_pos.split(":")[1]  # Position of the SNP
+    snp_full_pos = f"chr{chrom}:{pos}-{pos}"  # Full position of SNP in 'chr<chrom>:<pos>-<pos>' format, for tabix query
 
-if not args.output.endswith(".csv"):
-    sys.stderr.write("WARNING: Output file must be a CSV file. Adding extension '.csv' to output file name.\n")
-    args.output = args.output + ".csv"
+    sys.stdout.write(f"Requested position: <{snp_full_pos}>\n")
 
-# Create 'snp_full_pos' variable
-chr = args.snp_pos.split(":")[0]  # Chromosome of the SNP
-pos = args.snp_pos.split(":")[1]  # Position of the SNP
-snp_full_pos = f"chr{chr}:{pos}-{pos}"  # Full position of the SNP in format of chr12:62910804-62910804, for tabix query
+    # Check if ReMap file exists, it is bgzipped, and it has a tabix index file
+    if not os.path.isfile(remap_file):
+        raise ValueError("ReMap file does not exist")
+    if not remap_file.endswith(".gz"):
+        raise ValueError("ReMap file must be bgzipped")
+    if not os.path.isfile(remap_file + ".tbi"):
+        raise ValueError("No tabix index file could be found for the ReMap file.")
 
-print(f"Requested position: <{snp_full_pos}>")
+    # Check if output file is a CSV file. If not, add extension '.csv' to output file name.
+    if not output.endswith(".csv"):
+        sys.stderr.write("WARNING: Output file must be a CSV file. Adding extension '.csv' to output file name.\n")
+        output = output + ".csv"
 
-# Check if ReMap file exists
-if not os.path.isfile(args.remap_file):
-    raise ValueError("ReMap file does not exist")
+    # Start processing the tabix query output
+    # Compose tabix query
+    command = f"tabix {remap_file} {snp_full_pos}"
 
-# Check it is bgzipped
-if not args.remap_file.endswith(".gz"):
-    raise ValueError("ReMap file must be bgzipped")
+    # Run command and store output. Requires tabix.
+    query_output = os.popen(command).read()
 
-# Check that the tabix index file exists
-if not os.path.isfile(args.remap_file + ".tbi"):
-    raise ValueError("No tabix index file could be found for the ReMap file.")
+    # Check if the output is empty
+    if query_output == "":
+        raise ValueError("No ReMap entries found for this SNP.")
 
-remap_file = args.remap_file  # Full ReMap BED file, downloaded from the ReMap website
+    # Save the output of the tabix query to a file
+    if not tmp_dir:  # If no tmp_dir is specified, use the current working directory
+        tmp_dir = os.getcwd()
+    # Check if a directory named 'tabix_slices' exists in the tmp_dir. If not, create it.
+    if not os.path.isdir(f"{tmp_dir}/tabix_slices"):
+        os.mkdir(f"{tmp_dir}/tabix_slices")
+    tmp_file = f"remap2022_all_macs2_hg38_v1_0.{snp_full_pos}.bed"
+    tmp_file_path = f"{tmp_dir}/tabix_slices/{tmp_file}"
+    with open(tmp_file_path, "w") as f:
+        f.write(query_output)
 
-# Compose tabix query
-command = f"tabix {remap_file} {snp_full_pos}"
+    df = pl.read_csv(tmp_file_path, sep="\t",
+                     new_columns=["chrom", "start", "end", "name", "score", "strand", "thickStart", "thickEnd",
+                                  "itemRgb"])
 
-# Run command and store output. Requires tabix.
-output = os.popen(command).read()
+    # Distance of SNP to peak. We'll use this to sort entries later
+    # It'd probably be fine to calculate distance to thickStart, but we'll calculate the center of the peak to be safe
+    df = df.with_column(pl.struct(['thickStart', 'thickEnd'])
+                        .apply(lambda s: int((s['thickStart'] + s['thickEnd']) / 2))
+                        .alias("thickCenter"))
 
-# Check if the output is empty
-if output == "":
-    raise ValueError("No ReMap entries found for this SNP.")
+    # Calculate distance to SNP pos (stored in variable 'pos')
+    df = df.with_column(pl.col("thickCenter").apply(lambda s: abs(int(s) - int(pos))).alias("distance_to_peak"))
 
-# Save the output to a file
-tmp_file = f"data/tabix_slices/remap2022_all_macs2_hg38_v1_0.{snp_full_pos}.bed"
-with open(tmp_file, "w") as f:
-    f.write(output)
+    # Split the name column by '.' and make new columns out of the three entries
+    df = df.with_columns([pl.col("name").str.split(".").apply(lambda s: s[0]).alias("study_accession"),
+                          pl.col("name").str.split(".").apply(lambda s: s[1]).alias("transcription_factor"),
+                          pl.col("name").str.split(".").apply(lambda s: s[2]).alias("biotype")])
 
-df = pl.read_csv(tmp_file, sep="\t",
-                 new_columns=["chrom", "start", "end", "name", "score", "strand", "thickStart", "thickEnd", "itemRgb"])
+    out_df = df.select(["study_accession", "transcription_factor", "biotype", "distance_to_peak"])
 
-# Distance of SNP to peak. We'll use this to sort entries later.
-# It'd probably be fine to calculate distance to thickStart, but just to be safe we'll calculate the center of the peak.
-df = df.with_column(pl.struct(['thickStart', 'thickEnd'])
-                    .apply(lambda s: int((s['thickStart'] + s['thickEnd']) / 2))
-                    .alias("thickCenter"))
+    # Sort by distance from SNP to ChIP-seq peak.
+    out_df = out_df.sort("distance_to_peak")
 
-# Calculate distance to SNP pos (stored in variable 'pos')
-df = df.with_column(pl.col("thickCenter").apply(lambda s: abs(int(s) - int(pos))).alias("distance_to_peak"))
+    # Write to file
+    out_df.write_csv(output)
 
-# Split the name column by '.' and make new columns out of the three entries
-df = df.with_columns([pl.col("name").str.split(".").apply(lambda s: s[0]).alias("study_accession"),
-                      pl.col("name").str.split(".").apply(lambda s: s[1]).alias("transcription_factor"),
-                      pl.col("name").str.split(".").apply(lambda s: s[2]).alias("biotype")])
 
-out_df = df.select(["study_accession", "transcription_factor", "biotype", "distance_to_peak"])
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("chr_pos", help="Position of the SNP in the format <chr>:<pos>")
+    parser.add_argument("-r", "--remap_file", required=True,
+                        help="Path to ReMap BED file. Requires tabix index file.")
+    parser.add_argument("-t", "--tmp_dir", help="OPTIONAL. Directory to store temporary files. If none is specified, "
+                                                "temporary files will be stored in the current working directory.")
+    parser.add_argument("-o", "--output", required=True, help="Output file. Must be a CSV file.")
+    args = parser.parse_args()
 
-# Sort by distance from SNP to ChIP-seq peak.
-out_df = out_df.sort("distance_to_peak")
-
-# Write to file
-out_df.write_csv(args.output)
+    extract_studies_for_single_snp(args.chr_pos, args.remap_file, args.tmp_dir, args.output)
